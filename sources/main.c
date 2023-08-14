d7d88e92908e437256cb372a2ee3efd96eb8e5f4
ranguli <hello@joshmurphy.ca>
2023-08-14 17:31:16 -0230

    Working WIP with big old clang-format

--- sources/main.c
+++ sources/main.c
@@ -1,35 +1,137 @@
+#include "cLDtk.h"
 #include "raylib.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define SCREEN_WIDTH (800)
-#define SCREEN_HEIGHT (450)
+//--------------------------------------------------------------------------------------
+//                              Raylib specific functions
+//--------------------------------------------------------------------------------------
+void DrawSprite(Texture2D texture, Rectangle sourceRec, Vector2 position, int flip, Color tint);
+void DrawTiles(struct layerInstances *layer, Texture2D texture, int offset, Color tint);
+//--------------------------------------------------------------------------------------
 
-#define WINDOW_TITLE "Window title"
+int main(void) {
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 816;
+    const int screenHeight = 336;
+    InitWindow(screenWidth, screenHeight, "raylib LDtk loader - Load Tiles");
 
-int main(void)
-{
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
-    SetTargetFPS(60);
+    // NOTE: Textures MUST be loaded after Window initialization (OpenGL context is required)
+    Texture2D texture = LoadTexture(ASSETS_PATH "SunnyLand_by_Ansimuz.png"); // Texture loading
 
-    Texture2D texture = LoadTexture(ASSETS_PATH"test.png"); // Check README.md for how this works
+    // Sprite size offset
+    int offset = 16;
 
-    while (!WindowShouldClose())
+    SetTargetFPS(60); // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Load the JSON file
+    loadJSONFile("{\"jsonVersion\":\"\"}", ASSETS_PATH "map.json");
+
+    // Import the map data from the JSON file into memory
+    importMapData();
+
+    // Get level data from memory
+    struct levels *lvl_one;
+    lvl_one = getLevel("Level_1");
+
+    // Get foreground layer data from memory
+    struct layerInstances *lvl_one_collisions;
+    lvl_one_collisions = getLayer("Collisions", lvl_one->uid);
+
+    // Get background layer data from memory
+    struct layerInstances *lvl_one_background;
+    lvl_one_background = getLayer("Bg_textures", lvl_one->uid);
+
+    // Main game loop
+    while (!WindowShouldClose()) // Detect window close button or ESC key
     {
+        // Update
+        //----------------------------------------------------------------------------------
+
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
         BeginDrawing();
 
-        ClearBackground(RAYWHITE);
+        ClearBackground(BLACK);
 
-        const int texture_x = SCREEN_WIDTH / 2 - texture.width / 2;
-        const int texture_y = SCREEN_HEIGHT / 2 - texture.height / 2;
-        DrawTexture(texture, texture_x, texture_y, WHITE);
+        // Draw background
+        DrawTiles(lvl_one_background, texture, offset, WHITE);
 
-        const char* text = "OMG! IT WORKS!";
-        const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
-        DrawText(text, SCREEN_WIDTH / 2 - text_size.x / 2, texture_y + texture.height + text_size.y + 10, 20, BLACK);
+        // Draw foreground
+        DrawTiles(lvl_one_collisions, texture, offset, WHITE);
 
         EndDrawing();
+        //----------------------------------------------------------------------------------
     }
 
-    CloseWindow();
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    UnloadTexture(texture); // Texture unloading
+
+    freeMapData(); // Unload map data
+
+    json_value_free(schema);    // Unload JSON schema
+    json_value_free(user_data); // Unload JSON data
+
+    CloseWindow(); // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
 
     return 0;
 }
+
+//--------------------------------------------------------------------------------------
+//                              Raylib specific functions
+//--------------------------------------------------------------------------------------
+
+void DrawSprite(Texture2D texture, Rectangle sourceRec, Vector2 position, int flip, Color tint) {
+    Rectangle destRec = {position.x, position.y, fabsf(sourceRec.width), fabsf(sourceRec.height)};
+    Vector2 origin = {0.0f, 0.0f};
+
+    // flip=0;
+
+    switch (flip) {
+    case 0:
+        // flipx =false;
+        // flipy = false;
+        break;
+    case 1:
+        // flipx = true;
+        // flipy = false;
+        sourceRec.width *= -1;
+        break;
+
+    case 2:
+        // flipx = false;
+        // flipy = true;
+        sourceRec.height *= -1;
+        break;
+    case 3:
+        // flipx = true;
+        // flipy = true;
+        sourceRec.width *= -1;
+        sourceRec.height *= -1;
+
+        break;
+    }
+
+    DrawTexturePro(texture, sourceRec, destRec, origin, 0.0f, tint);
+}
+
+void DrawTiles(struct layerInstances *layer, Texture2D texture, int offset, Color tint) {
+
+    for (int y = layer->autoTiles_data_ptr->count; y-- > 0;) {
+
+        DrawSprite(texture,
+                   (Rectangle){(float)layer->autoTiles_data_ptr[y].SRCx,
+                               (float)layer->autoTiles_data_ptr[y].SRCy, offset, offset},
+                   (Vector2){(float)layer->autoTiles_data_ptr[y].x, (float)layer->autoTiles_data_ptr[y].y},
+                   layer->autoTiles_data_ptr[y].f, tint);
+    }
+}
+//--------------------------------------------------------------------------------------
